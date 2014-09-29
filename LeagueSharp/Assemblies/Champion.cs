8efc8022c24ddea7b9d7b1cf63dd7a55dd32fed1
iJavai <iJava.i@hotmail.co.uk>
2014-09-29 04:04:34 +0100

    sdadadada

--- LeagueSharp/Assemblies/Champion.cs
+++ LeagueSharp/Assemblies/Champion.cs
@@ -1,20 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LeagueSharp;
+﻿using LeagueSharp;
 using LeagueSharp.Common;
 
 namespace Assemblies {
-    class Champion : ChampionUtils {
+    internal abstract class Champion : ChampionUtils {
+        protected readonly Obj_AI_Hero player = ObjectManager.Player;
+        protected Spell E;
+        protected Spell Q;
+        protected Spell R;
+        protected Spell W;
+        protected Menu menu;
+        protected Orbwalking.Orbwalker orbwalker;
 
-        public readonly Obj_AI_Hero player = ObjectManager.Player;
-        private Menu menu;
-        private Orbwalking.Orbwalker orbwalker;
-
-        public Champion() {
-            Game.PrintChat(player.ChampionName+" loaded");
+        protected Champion() {
+            Game.PrintChat(player.ChampionName + " loaded");
             addBasicMenu();
         }
 
@@ -28,5 +26,6 @@ namespace Assemblies {
             menu.Item("FarmDelay").SetValue(new Slider(0, 0, 200));
             menu.AddToMainMenu();
         }
+
     }
 }
--- LeagueSharp/Assemblies/Program.cs
+++ LeagueSharp/Assemblies/Program.cs
@@ -4,9 +4,8 @@ using LeagueSharp;
 using LeagueSharp.Common;
 
 namespace Assemblies {
-    internal class Program {
-        public static Champion champion;
-        public static Menu menu;
+    internal static class Program {
+        private static Champion champion;
 
         private static void Main(string[] args) {
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
