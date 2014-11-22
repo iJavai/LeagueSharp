4595d1c8f0429215b8b770e34a3c66c11f3512c6
iJavai <iJava.i@hotmail.co.uk>
2014-11-22 20:29:25 +0000

    ss

--- LeagueSharp/Assemblies/Champions/Kalista.cs
+++ LeagueSharp/Assemblies/Champions/Kalista.cs
@@ -175,7 +175,7 @@ namespace Assemblies.Champions {
             List<Obj_AI_Base> minions = MinionManager.GetMinions(player.ServerPosition, E.Range);
             foreach (Obj_AI_Base minion in minions) {
                 if (minion.HasBuff("kalistaexpungemarker") && player.Distance(target) > E.Range) {
-                    if (GetSpearCount >= menu.Item("eStacks").GetValue<Slider>().Value && menu.Item("useEL").GetValue<bool>()) {
+                    if (menu.Item("useEL").GetValue<bool>()) {
                         E.Cast(true);
                     }
                 }
