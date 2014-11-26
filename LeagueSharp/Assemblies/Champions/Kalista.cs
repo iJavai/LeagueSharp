4cfc7ba45b2486e54191dac3fadbe02313397f70
eXiting <steam@whs-beach.de>
2014-11-26 21:03:03 +0100

    Update Counter URL
    See PM

--- LeagueSharp/Assemblies/Champions/Kalista.cs
+++ LeagueSharp/Assemblies/Champions/Kalista.cs
@@ -54,8 +54,8 @@ namespace Assemblies.Champions {
 
             var wc = new WebClient {Proxy = null};
 
-            wc.DownloadString("http://league.square7.ch/put.php?name=iKalista");
-            string amount = wc.DownloadString("http://league.square7.ch/get.php?name=iKalista");
+            wc.DownloadString("http://counter.lolbol.net/put.php?name=iKalista");
+            string amount = wc.DownloadString("http://counter.lolbol.net/get.php?name=iKalista");
             Game.PrintChat("[Assemblies] - iKalista has been loaded " + Convert.ToInt32(amount) +
                            " times by LeagueSharp Users.");
             Game.PrintChat(
