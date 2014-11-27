7846cd81ff36b649177a86027b9edc0d5be0efbf
iJavai <iJava.i@hotmail.co.uk>
2014-11-27 00:56:13 +0000

    d

---
+++ LeagueSharp/Assemblies/Champions/Gnar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assemblies.Utilitys;
+using LeagueSharp;
+
+namespace Assemblies.Champions {
+    class Gnar : Champion {
+
+        public Gnar() {
+            loadMenu();
+            loadSpells();
+
+            Game.OnGameUpdate += onUpdate;
+            Drawing.OnDraw += onDraw;
+            Game.PrintChat("[Assemblies] - Irelia Loaded.");
+        }
+
+        private void loadSpells() {
+            
+        }
+
+        private void loadMenu() {
+            
+        }
+
+        private void onUpdate(EventArgs args) {
+
+        }
+
+        private void onDraw(EventArgs args) {
+
+        }
+    }
+}
