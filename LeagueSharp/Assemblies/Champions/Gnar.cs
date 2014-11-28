94fa8a173563c677212d80c6e0e2b82cfb72205c
iJavai <iJava.i@hotmail.co.uk>
2014-11-28 19:10:29 +0000

    d

--- LeagueSharp/Assemblies/Champions/Gnar.cs
+++ LeagueSharp/Assemblies/Champions/Gnar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Assemblies.Utilitys;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
@@ -41,13 +42,15 @@ namespace Assemblies.Champions {
         private void loadMenu() {
             menu.AddSubMenu(new Menu("Combo Options", "combo"));
             menu.SubMenu("combo").AddItem(new MenuItem("useQC", "Use Q in combo").SetValue(true));
+            menu.SubMenu("combo").AddItem(new MenuItem("useWC", "Use W in combo").SetValue(true));
             menu.SubMenu("combo").AddItem(new MenuItem("useEC", "Use E in combo").SetValue(true));
             menu.SubMenu("combo").AddItem(new MenuItem("useRC", "Use R in combo").SetValue(true));
             menu.SubMenu("combo").AddItem(
                 new MenuItem("minEnemies", "Enemies for R").SetValue(new Slider(2, 1, 5)));
 
             menu.AddSubMenu(new Menu("Harass Options", "harass"));
-            menu.SubMenu("harass").AddItem(new MenuItem("useQH", "Use Q in combo").SetValue(true));
+            menu.SubMenu("harass").AddItem(new MenuItem("useQH", "Use Q in harass").SetValue(true));
+            menu.SubMenu("harass").AddItem(new MenuItem("useWH", "Use W in harass").SetValue(true));
             menu.SubMenu("harass").AddItem(new MenuItem("useEH", "Use E in harass").SetValue(false));
 
             menu.AddSubMenu(new Menu("Laneclear Options", "laneclear"));
@@ -71,12 +74,56 @@ namespace Assemblies.Champions {
             menu.SubMenu("misc").AddItem(
                 new MenuItem("throwPos", "Position to throw enemies").SetValue(
                     new StringList(new[] {"Closest Wall", "Mouse Position"})));
+            menu.SubMenu("misc").AddItem(new MenuItem("alwaysR", "Always Ult if killable").SetValue(true));
+
         }
 
         private void onUpdate(EventArgs args) {
             if (player.IsDead) return;
 
-            Console.WriteLine("gi");
+            Obj_AI_Hero target = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Physical);
+
+            switch (xSLxOrbwalker.CurrentMode) {
+                case xSLxOrbwalker.Mode.Combo:
+                    doCombo(target);
+                    break;
+            }
+        }
+
+        private void doCombo(Obj_AI_Hero target) {
+            //TODO le combo modes
+            if (Q.IsReady() && target.IsValidTarget(Q.Range) &&
+                Q.GetPrediction(target, true).Hitchance >= HitChance.Medium) {
+                if (isMenuEnabled(menu, "useQC"))
+                    Q.Cast(target, true, true);
+            }
+            if (qMega.IsReady() && target.IsValidTarget(qMega.Range) &&
+                qMega.GetPrediction(target).Hitchance >= HitChance.Medium) {
+                if (isMenuEnabled(menu, "useQC"))
+                    qMega.Cast(target, true);
+            }
+
+            if (W.IsReady() && target.IsValidTarget(W.Range) && player.Distance(target) < W.Range) {
+                if (isMenuEnabled(menu, "useWC"))
+                    W.Cast(target, true);
+            }
+
+            if (E.IsReady() && target.IsValidTarget(E.Range)) {
+                if (isMenuEnabled(menu, "useEC"))
+                    E.Cast(target, true);
+            }
+
+            if (R.IsReady() && target.IsValidTarget(R.Range)) {
+                if (isMenuEnabled(menu, "useRC")) {
+                    if (player.GetSpellDamage(target, SpellSlot.R) >= target.Health) {
+                        R.Cast(target, true);
+                    }
+                    else {
+                        castR(target);
+                    }
+                }
+                    
+            }
         }
 
         private void castR(Obj_AI_Hero target) {
@@ -115,18 +162,17 @@ namespace Assemblies.Champions {
         }
 
         private void unitFlee() {
-            if (E.IsReady() || eMega.IsReady()) {
-                const float distance = 300;
+            if (!E.IsReady() && !eMega.IsReady()) return;
+            const float distance = 300;
 
-                foreach (
-                    Obj_AI_Base minion in
-                        MinionManager.GetMinions(player.Position, E.Range, MinionTypes.All, MinionTeam.All,
-                            MinionOrderTypes.None).Where(minion => minion != null && minion.IsValid).Where(
-                                minion =>
-                                    Vector3.DistanceSquared(player.Position, Game.CursorPos) <= distance*distance &&
-                                    Vector3.DistanceSquared(player.Position, minion.Position) <= E.Range*E.Range)) {
-                    E.Cast(minion, true);
-                }
+            foreach (
+                Obj_AI_Base minion in
+                    MinionManager.GetMinions(player.Position, E.Range, MinionTypes.All, MinionTeam.All,
+                        MinionOrderTypes.None).Where(minion => minion != null && minion.IsValid).Where(
+                            minion =>
+                                Vector3.DistanceSquared(player.Position, Game.CursorPos) <= distance*distance &&
+                                Vector3.DistanceSquared(player.Position, minion.Position) <= E.Range*E.Range)) {
+                E.Cast(minion, true);
             }
         }
 
